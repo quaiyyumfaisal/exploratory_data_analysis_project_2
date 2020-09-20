aec083a6224b0972c1876a6e5c32421b2c1ead72
Faisal Quaiyyum <70777501+quaiyyumfaisal@users.noreply.github.com>
2020-09-20 20:57:23 +0600

    plot5.R

--- plot5.R
+++ plot5.R
@@ -8,22 +8,24 @@ if(!exists("SCC")){
     SCC <- readRDS("Source_Classification_Code.rds")
 }
 
+
 ## Question 5: How have emissions from motor vehicle sources changed from 1999 to 2008 in Baltimore City?
 
 library(ggplot2)
 library(dplyr)
 
+
 ## Constructing and saving the plot in png format.
 
-scc_vehicles <- SCC[grep("[Vv]ehicle", SCC$EI.Sector), "SCC"]
-vehicle_emissions <- NEI %>%
-    filter(SCC %in% scc_vehicles & fips == "24510") %>%
-    group_by(year) %>%
-    summarise(Emissions = sum(Emissions))
+baltcitymary.emissions<-NEI[(NEI$fips=="24510") & (NEI$type=="ON-ROAD"),]
 
-png("plot5.png", width = 480, height = 480)
-g <- ggplot(coal_summary, aes(x=year, y=round(Emissions/1000,2), label = round(Emissions/1000,2), fill = year))
-v_plot <- g+geom_bar(stat = "identity")+xlab("Year")+ylab(expression('PM'[2.5]*' Emissions in Kilotons'))+
-    geom_label(aes(fill=year), color ="white", fontface = "bold")+ggtitle("Vehicles Emissions in Baltimore City from 1999 to 2008")
-print(v_plot)
+baltcitymary.emissions.byyear <- summarise(group_by(baltcitymary.emissions, year), Emissions=sum(Emissions))
+
+png("plot5.png")
+ggplot(baltcitymary.emissions.byyear, aes(x=factor(year), y=Emissions,fill=year, label = round(Emissions,2))) +
+    geom_bar(stat="identity") +
+    xlab("year") +
+    ylab(expression("total PM"[2.5]*" emissions in tons")) +
+    ggtitle("Emissions from motor vehicle sources in Baltimore City")+
+    geom_label(aes(fill = year),colour = "white", fontface = "bold")
 dev.off()
